6a881c3e949bbf2858602cc2192127cdb7cd51c8
simon <hanzhenfeng@wuxicloud.com>
2021-03-07 21:55:32 +0800

    refactor: file util 重构

--- src/main/java/cn/codingstyle/spider/FileUtil.java
+++ src/main/java/cn/codingstyle/spider/FileUtil.java
@@ -2,18 +2,14 @@ package cn.codingstyle.spider;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RequestCallback;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -23,47 +19,41 @@ import java.util.Arrays;
 @Slf4j
 public class FileUtil {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private static final String IMAGES_DIR = "images/";
+
+    private final RestTemplate restTemplate;
 
     public FileUtil(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+        createParentPath();
+    }
+
+    private void createParentPath() {
+        new File(IMAGES_DIR).mkdirs();
     }
 
     public File downloadFile(String url) throws Exception {
         log.info("start download file from: {}", url);
         Instant start = Instant.now();
-        String targetPath = createParentPath() + url.substring(url.lastIndexOf("/") + 1);
-        RequestCallback requestCallback = request -> request.getHeaders()
-            .setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM, MediaType.ALL));
+        String targetPath = IMAGES_DIR + url.substring(url.lastIndexOf("/") + 1);
         deleteFile(targetPath);
-        restTemplate.execute(URI.create(url), HttpMethod.GET, requestCallback, clientHttpResponse -> {
-            Files.copy(clientHttpResponse.getBody(), Paths.get(targetPath));
-            return null;
-        });
+        restTemplate.execute(
+            URI.create(url),
+            HttpMethod.GET,
+            request -> request.getHeaders()
+                .setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM, MediaType.ALL)),
+            clientHttpResponse -> {
+                Files.copy(clientHttpResponse.getBody(), Paths.get(targetPath));
+                return null;
+            }
+        );
         log.info("file download success, cost time: {} ms", ChronoUnit.MILLIS.between(start, Instant.now()));
         return new File(targetPath);
     }
 
-    private String createParentPath() throws IOException {
-        String parentPath = "images/";
-        Path path = Paths.get(parentPath);
-        if (Files.notExists(path)) {
-            Files.createDirectories(path);
-        }
-        return parentPath;
-    }
-
-    private void deleteFile(String targetPath) throws IOException {
-        log.info("targetPath = {}", targetPath);
+    private void deleteFile(String targetPath) {
+        log.debug("targetPath = {}", targetPath);
         File existedFile = new File(targetPath);
-        if (!existedFile.exists()) {
-            return;
-        }
-        if (existedFile.isDirectory()) {
-            FileUtils.deleteDirectory(existedFile);
-        } else {
-            existedFile.delete();
-        }
+        FileUtils.deleteQuietly(existedFile);
     }
 }
--- src/test/java/cn/codingstyle/spider/FileUtilTest.java
+++ src/test/java/cn/codingstyle/spider/FileUtilTest.java
@@ -1,5 +1,6 @@
 package cn.codingstyle.spider;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -17,17 +19,22 @@ public class FileUtilTest {
     private FileUtil fileUtil;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
+        deleteImagesDir();
         RestTemplate restTemplate = new RestTemplate();
         fileUtil = new FileUtil(restTemplate);
     }
 
-    @AfterEach
-    void tearDown() {
+    private void deleteImagesDir() throws IOException {
         File dir = new File("images");
         if (dir.exists()) {
-            dir.delete();
+            FileUtils.forceDelete(dir);
         }
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        deleteImagesDir();
     }
 
     @Test
@@ -42,7 +49,6 @@ public class FileUtilTest {
         assertThat(file.length()).isEqualTo(34390);
         assertThat(file.getName()).isEqualTo("4790087-0a958b58ad2c6511.png");
 
-        oldFile.delete();
     }
 
     @Test
@@ -53,17 +59,14 @@ public class FileUtilTest {
 
     @Test
     public void should_delete_existed_file_befere_download() throws Exception {
-        File dir = new File("images");
-        assertThat(dir.mkdirs()).isTrue();
-        File fakeFile = new File(dir, "4790087-0a958b58ad2c6511.png");
-        assertThat(fakeFile.createNewFile()).isTrue();
-        assertThat(fakeFile.length()).isEqualTo(0);
+//        File dir = new File("images");
+//        dir.mkdirs();
+//        File fakeFile = new File(dir, "4790087-0a958b58ad2c6511.png");
+//        assertThat(fakeFile.createNewFile()).isTrue();
+//        assertThat(fakeFile.length()).isEqualTo(0);
 
         File file = fileUtil.downloadFile("https://upload-images.jianshu.io/upload_images/4790087-0a958b58ad2c6511.png");
-        assertThat(file).isNotNull();
         assertThat(file.length()).isEqualTo(34390);
         assertThat(file.getName()).isEqualTo("4790087-0a958b58ad2c6511.png");
-
-        file.delete();
     }
 }
